f6f17ac5f91084e6d37cd002ebfd263e7d97acbd
carlofav12 <carlofav12@gmail.com>
2024-10-04 18:52:38 -0500

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AppTransaccion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
